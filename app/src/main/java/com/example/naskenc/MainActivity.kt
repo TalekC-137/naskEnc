d3fa12eb8cd59b4285714e3f384b1602898d9e98
TalekC-137 <pawatl2002@gmail.com>
2021-07-25 15:44:52 +0200

    added finger print auth, design, reversed recyclerview

--- app/src/main/java/com/example/naskenc/MainActivity.kt
+++ app/src/main/java/com/example/naskenc/MainActivity.kt
@@ -1,23 +1,20 @@
 package com.example.naskenc
 
+import android.content.Context
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isEmpty
-import androidx.security.crypto.EncryptedFile
-import androidx.security.crypto.MasterKey
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 import com.example.naskenc.fetcher.CryptedNotesRow
 import com.example.naskenc.models.CryptedModel
-import kotlinx.android.synthetic.main.activity_main.*
-import java.io.*
-import java.nio.charset.StandardCharsets
-import javax.crypto.Cipher
-import javax.crypto.KeyGenerator
-import javax.crypto.SecretKey
 import com.xwray.groupie.GroupieAdapter
-import com.xwray.groupie.GroupieViewHolder
-import com.xwray.groupie.Item
+import kotlinx.android.synthetic.main.activity_main.*
 import net.sqlcipher.database.SQLiteDatabase
+import java.io.*
+import java.security.AccessController.getContext
+import java.util.concurrent.Executor
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,22 +22,63 @@ class MainActivity : AppCompatActivity() {
 
     val adapter = GroupieAdapter()
 
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         SQLiteDatabase.loadLibs(this);
 
+        executor = ContextCompat.getMainExecutor(this)
+        biometricPrompt = BiometricPrompt(this, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int,
+                                                   errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    Toast.makeText(applicationContext,
+                        "Authentication error: $errString", Toast.LENGTH_SHORT)
+                        .show()
+                }
+                override fun onAuthenticationSucceeded(
+                    result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    getDatabase()  //decrypts and shows the the database
+                    Toast.makeText(applicationContext,
+                        "Authentication succeeded!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(applicationContext, "Authentication failed",
+                        Toast.LENGTH_SHORT)
+                        .show()
+                }
+            })
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric login for my app")
+            .setSubtitle("Log in using your biometric credential")
+            .setNegativeButtonText("cancel")
+            .build()
+
+
         rv_messages.adapter = adapter
 
         btn_encrypt.setOnClickListener {
-
             addtoDatabase()
+            val imm: InputMethodManager =
+                baseContext.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(et_message.getWindowToken(), 0)
+
         }
         btn_decrypt.setOnClickListener {
-
-            getDatabase()
-
+            biometricPrompt.authenticate(promptInfo)
+            val imm: InputMethodManager =
+                baseContext.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(et_message.getWindowToken(), 0)
         }
 
     }
@@ -107,6 +145,8 @@ class MainActivity : AppCompatActivity() {
             val status = databaseHelper.addRow(CryptedModel(0, et_title.text.toString(), et_message.text.toString()))
             if (status > -1) {
                 Toast.makeText(applicationContext, "note saved", Toast.LENGTH_LONG).show()
+                et_message.text.clear()
+                et_title.text.clear()
             }
         }else{
             Toast.makeText(this, "uzupełnij oba pola", Toast.LENGTH_SHORT).show()
